fca4d6e37b6bd4ce5a33ff28d5e98d7552dbd282
Try-Polar <eb1033@york.ac.uk>
2017-05-12 10:41:26 +0100

    Add project files.

---
+++ RoutingPrototype/BaseEntity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace RoutingPrototype
+{
+    abstract class BaseEntity
+    {
+        private Texture2D mTexture;
+        private String mTexturePath;
+        private Vector2 mPos;
+        private Rectangle mRectangle;
+
+        public BaseEntity(Texture2D texture, Vector2 initialPosition)
+        {
+            this.mTexture = texture;
+            this.mPos = initialPosition;
+            this.mRectangle = new Rectangle((int)mPos.X, (int)mPos.Y, mTexture.Width, mTexture.Height);
+        }
+
+        public Vector2 Position
+        {
+            get { return mPos; }
+            set { mPos = value; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return mTexture; }
+            set { mTexture = value; }
+        }
+
+        public Rectangle Rect
+        {
+            get { return mRectangle; }
+            set { mRectangle = value; }
+        }
+
+        public void setRectanglePos(Vector2 pos)
+        {
+            mRectangle.X = (int)pos.X;
+            mRectangle.Y = (int)pos.Y;
+        }
+
+
+        public abstract void Update(GameTime gameTime);
+
+        public abstract void Draw(SpriteBatch spriteBatch);
+
+
+    }
+}
---
+++ RoutingPrototype/RoutingPrototype.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RoutingPrototype
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class RoutingPrototype : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        Texture2D podTexture;
+
+        MovingEntity test;
+
+        public RoutingPrototype()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            podTexture = Content.Load<Texture2D>("Pod");
+
+            test = new MovingEntity(podTexture, Vector2.Zero);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+            Content.Unload();
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            test.Update(gameTime);
+            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            test.Draw(spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
