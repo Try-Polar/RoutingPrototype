6b865b582b1a5ba714382010328f489a2ecc90d2
Try-Polar <eb1033@york.ac.uk>
2017-05-20 14:44:45 +0100

    Got the basic formation back-end stuff down
    Woooooooo

---
+++ RoutingPrototype/FormationManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RoutingPrototype
+{
+    class FormationManager
+    {
+        Skein mCurrentSkein;
+
+        Texture2D mFormationPodTexture;
+
+        FormationPod[] mFormationPods; //could use a list but I I feel limiting the size of the skein shown (so that it can fit in the area I have assigned to it) would be beneficial
+                                       //I'm also going to use it to do work something similar to a data array into the function of the formation manager
+        List<int> mFreeIndices;
+
+        public FormationManager(Texture2D texture)
+        {
+            mCurrentSkein = null;
+            mFormationPodTexture = texture;
+            mFormationPods = new FormationPod[10];
+            mFreeIndices = new List<int>();
+        }
+
+        //Add new pod to the formation (this one should be easy, just spawn a new pod offscreen and have it move into place)
+        public void addPod()
+        {
+
+        }
+
+        //Remove a randomly chosen pod from the formation (this might be slightly more difficult since I can't just make the pod disapear I've gotta make it move out of formation and offscreen
+        // and then I have to make the others reform, but that should be easy  I'll just readjust the formation indices)
+        public void removePod()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ RoutingPrototype/FormationPod.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RoutingPrototype
+{
+    class FormationPod : MovingEntity, IUpdateDraw
+    {
+        Vector2 mLeaderPostion;
+        Vector2 mOffset;
+        int mFormationIndex;
+        float mOffsetSpacing;
+        float mMass = 0.05f;
+        float VELOCITY = 50;
+
+        public FormationPod(Texture2D texture, Vector2 initialPosition, int formationIndex) : base(texture, initialPosition)
+        {
+            mFormationIndex = formationIndex;
+            mOffset = calculateOffset();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Vector2 acceleration = arrive(mOffset) / mMass;
+            Velocity += acceleration * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (Velocity.Length() > maxVelocity)
+            {
+                Velocity *= maxVelocity / Velocity.Length();
+            }
+
+            Position = Position + (Velocity * (float)gameTime.ElapsedGameTime.TotalSeconds) * VELOCITY;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        void changeFormationIndex(int newIndex)
+        {
+            mFormationIndex = newIndex;
+            mOffset = calculateOffset();
+        }
+
+        Vector2 calculateOffset()
+        {
+            Vector2 offset = new Vector2(1, 1);
+            //if index is 0 then pod is leader and so does not have any offset
+            if (mFormationIndex == 0)
+            {
+                return Vector2.Zero;
+            }
+            else
+            {
+                int side = 1;
+                int offsetMultiplier = 0;
+                if ((mFormationIndex % 2) == 1) //if index is odd then offset will be put to the left else it will be on the right
+                {
+                    side *= -1;
+                    offsetMultiplier = (mFormationIndex / 2) + 1; //Pretty sure C# will always round down, so I'm adding 1 (I want 1/2 to be 1 and 3/2 to be 2 etc)
+                }
+                else
+                {
+                    offsetMultiplier = mFormationIndex / 2;
+                }
+                offset.X *= side;
+                offset *= offsetMultiplier;
+            }
+            return offset;
+        }
+
+    }
+}
